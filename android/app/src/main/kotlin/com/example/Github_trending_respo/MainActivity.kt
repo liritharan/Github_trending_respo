a98fced12bfc9425af3f7eeed8b796d6d13aea37
liritharan <56247562+liritharan@users.noreply.github.com>
2020-06-02 08:27:25 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/Github_trending_respo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Github_trending_respo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/api.dart
@@ -0,0 +1,76 @@
+import 'package:github_search/repo.dart';
+import 'package:date_format/date_format.dart';
+
+import 'dart:convert' show json, utf8;
+import 'dart:io';
+import 'dart:async';
+
+class Api {
+  static final HttpClient _httpClient = HttpClient();
+  static final String _url = "api.github.com";
+
+  static Future<List<Repo>> getRepositoriesWithSearchQuery(String query) async {
+    final uri = Uri.https(_url, '/search/repositories', {
+      'q': query,
+      'sort': 'stars',
+      'order': 'desc',
+      'page': '0',
+      'per_page': '25'
+    });
+
+    final jsonResponse = await _getJson(uri);
+    if (jsonResponse == null) {
+      return null;
+    }
+    if (jsonResponse['errors'] != null) {
+      return null;
+    }
+    if (jsonResponse['items'] == null) {
+      return List();
+    }
+
+    return Repo.mapJSONStringToList(jsonResponse['items']);
+  }
+
+  static Future<List<Repo>> getTrendingRepositories() async {
+    final lastMonth = DateTime.now().subtract(Duration(days: 30));
+    final formattedDate = formatDate(lastMonth, [yyyy, '-', mm, '-', dd]);
+
+    final uri = Uri.https(_url, '/search/repositories', {
+      'q': 'created:>$formattedDate',
+      'sort': 'stars',
+      'order': 'desc',
+      'page': '0',
+      'per_page': '300'
+    });
+
+    final jsonResponse = await _getJson(uri);
+    if (jsonResponse == null) {
+      return null;
+    }
+    if (jsonResponse['errors'] != null) {
+      return null;
+    }
+    if (jsonResponse['items'] == null) {
+      return List();
+    }
+
+    return Repo.mapJSONStringToList(jsonResponse['items']);
+  }
+
+  static Future<Map<String, dynamic>> _getJson(Uri uri) async {
+    try {
+      final httpRequest = await _httpClient.getUrl(uri);
+      final httpResponse = await httpRequest.close();
+      if (httpResponse.statusCode != HttpStatus.OK) {
+        return null;
+      }
+
+      final responseBody = await httpResponse.transform(utf8.decoder).join();
+      return json.decode(responseBody);
+    } on Exception catch (e) {
+      print('$e');
+      return null;
+    }
+  }
+}
